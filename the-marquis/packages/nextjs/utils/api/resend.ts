1b0aea2ded3fe28312a0bc59083f232447f99730
Gianfranco99 <gianfranco.benvenuto99gmail.com>
2024-12-17 09:49:20 +0000

    delete folder the-marquis

--- the-marquis/packages/nextjs/utils/api/resend.ts
+++
@@ -1,7 +0,0 @@
-import api from ".";
-import { IResend } from "./type";
-async function Resend(data: IResend): Promise<any> {
-  return await api.post("auth/resend-verification-email", data);
-}
-
-export { Resend };
--- the-marquis/packages/nextjs/utils/api/signup.ts
+++
@@ -1,7 +0,0 @@
-import api from ".";
-import { ISignup } from "./type";
-async function signUp(data: ISignup): Promise<any> {
-  return await api.post("auth/signup", data);
-}
-
-export { signUp };
